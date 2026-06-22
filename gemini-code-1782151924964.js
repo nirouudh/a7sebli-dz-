7ebed934ec6369f8fa6077f20e6a242ab8a73b8b
dahou.abdennour <dahouabdennour111@gmail.com>
2026-06-22 19:13:24 +0100

    Add files via upload

---
+++ gemini-code-1782151924964.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = 'lmd-calc-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './knight_flowers_wind_loop.mp4',
+  './guide.jpg',
+  './reelsvideo.io_1781973559904.mp3',
+  './reelsvideo.io_1781978267153.mp3',
+  './reelsvideo.io_1781973704823.mp3',
+  './reelsvideo.io_1781980214150.mp3'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => {
+      return res || fetch(e.request);
+    })
+  );
+});
